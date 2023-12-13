a7577289ef569f7a6a9404e31943e2c7b7f5a6bf
ToniPerea <apepa31@gmail.com>
2023-12-13 19:07:38 +0100

    Create canva and draw black rectangle

---
+++ src/main.js
@@ -0,0 +1,26 @@
+// Initialize canvas
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
+
+const BLOCK_SIZE = 20
+const BOARD_WIDTH = 14
+const BOARD_HEIGHT = 30
+
+canvas.width = BLOCK_SIZE * BOARD_WIDTH
+canvas.height = BLOCK_SIZE * BOARD_HEIGHT
+
+context.scale(BLOCK_SIZE, BLOCK_SIZE)
+
+// game loop
+// 2. game loop
+function update () {
+    draw()
+    window.requestAnimationFrame(update)
+}
+
+function draw () {
+    context.fillStyle = '#000'
+    context.fillRect(0, 0, canvas.width, canvas.height)
+}
+
+update()
